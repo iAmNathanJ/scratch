d0a9f4bf462d3fbcf9a41780f00ed2c11339a4b7
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-24 15:52:29 -0500

    working on new solution

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,38 +1,86 @@
 class GameOfLife
 
-  attr_reader :state
-
-  CELL = {
-    "create_id" => -> *coords { "#{coords[0]}:#{coords[1]}" },
-    "x" => -> cell_id { cell_id.split(":")[0] },
-    "y" => -> cell_id { cell_id.split(":")[1] }
-  }
-
   def initialize(state)
-    @state = state
-    @neighbors = []
+    @life_list = []
+    @dead_list = []
+    @neighbor_list = {}
+    @cell = {
+      :make => -> coords { "#{coords[:x]}:#{coords[:y]}" },
+      :x => -> cell { cell.split(":")[0].to_i },
+      :y => -> cell { cell.split(":")[1].to_i }
+    }
+    state.each { |coords| @life_list << cell(:make, coords) }
   end
 
-  def continue?(cell)
-    neighbors = get_neighbor_count(cell)
-    if cell && neighbors > 1 && neighbors < 4
-      true
-    elsif neighbors === 3
-      false
+  def cell(fn, args)
+    @cell[fn].call(args)
+  end
+
+  def continue?(c)
+    neighbors = @neighbor_list[c] || 0
+    alive?(c) ?
+      (neighbors > 1 && neighbors < 4) :
+      (neighbors === 3)
+  end
+
+  def alive?(c)
+    @life_list.include?(c)
+  end
+
+  def add_neighbors(c)
+    cs = []
+    for i in (-1..1) do
+      for j in (-1..1) do
+        if(i != 0 && j != 0)
+          cs << cell(:make, {x: cell(:x, c)-1, y: cell(:y, c)-1})
+        end
+      end
     end
+    # [
+    #   cell(:make, {x: cell(:x, c)-1, y: cell(:y, c)-1}),
+    #   cell(:make, {x: cell(:x, c)-1, y: cell(:y, c)  }),
+    #   cell(:make, {x: cell(:x, c)-1, y: cell(:y, c)+1}),
+    #   cell(:make, {x: cell(:x, c)  , y: cell(:y, c)  }),
+    #   cell(:make, {x: cell(:x, c)  , y: cell(:y, c)  }),
+    #   cell(:make, {x: cell(:x, c)+1, y: cell(:y, c)-1}),
+    #   cell(:make, {x: cell(:x, c)+1, y: cell(:y, c)  }),
+    #   cell(:make, {x: cell(:x, c)+1, y: cell(:y, c)+1})
+    # ]
+    cs.each do |c|
+      @neighbor_list[c] = 1
+    end
+  end
+
+  def build_neighbors
+    @life_list.each do |c|
+      add_neighbors(c)
+    end
+    p @neighbor_list
   end
 
   def generate
     next_gen = []
-    state.each do |cell|
-      next_gen << cell if continue? cell
-    end
+    build_neighbors
+    @state.each { |c| next_gen << c if continue?(c) }
     next_gen
   end
 
-  private
-  def get_neighbor_count(cell)
-    0
+  def state
+    @life_list.map do |c|
+      {
+        x: cell(:x, c),
+        y: cell(:y, c)
+      }
+    end
   end
 
 end
+
+life = GameOfLife.new([
+  { x: 0, y: 0 },
+  { x: 1, y: 0 },
+  { x: 0, y: 1 },
+  { x: 1, y: 1 }
+])
+
+life.build_neighbors
--- test.rb
+++ test.rb
@@ -6,38 +6,42 @@ class TestGameOfLife < Test::Unit::TestCase
   attr_reader :life
 
   def setup
-    @cells = [
+    state = [
       { x: 0, y: 0 },
       { x: 1, y: 0 },
       { x: 0, y: 1 },
       { x: 1, y: 1 }
     ]
-    @life = GameOfLife.new(@cells)
+    @life = GameOfLife.new(state)
   end
 
   def test_live_cell_continues_when_it_has_2_neighbors
-    cell = life.state[0]
-    p life.CELL[:create_id](cell)
+    coords = life.state[0]
+    cell = life.cell(:make, coords)
     assert( life.continue?(cell) )
   end
 
   def test_live_cell_continues_when_it_has_3_neighbors
-    cell = life.state[0]
+    coords = life.state[0]
+    cell = life.cell(:make, coords)
     assert( life.continue?(cell) )
   end
 
   def test_live_cell_dies_when_it_has_less_than_2_neighbors
-    cell = life.state[0]
+    coords = life.state[0]
+    cell = life.cell(:make, coords)
     assert( !life.continue?(cell) )
   end
 
   def test_live_cell_dies_when_it_has_more_than_3_neighbors
-    cell = life.state[0]
+    coords = life.state[0]
+    cell = life.cell(:make, coords)
     assert( !life.continue?(cell) )
   end
 
   def test_dead_cell_is_created_when_it_has_3_neighbors
-    cell = life.state[0]
+    coords = life.state[0]
+    cell = life.cell(:make, coords)
     assert( life.continue?(cell) )
   end
 
