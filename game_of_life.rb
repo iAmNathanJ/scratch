a848b4bca876a9c066c780d39ea308d2b66450d5
iAmNathanJ <jacobs@heysparkbox.com>
2016-02-24 13:33:03 -0500

    some stuff

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,3 +1,38 @@
 class GameOfLife
 
+  attr_reader :state
+
+  CELL = {
+    "create_id" => -> *coords { "#{coords[0]}:#{coords[1]}" },
+    "x" => -> cell_id { cell_id.split(":")[0] },
+    "y" => -> cell_id { cell_id.split(":")[1] }
+  }
+
+  def initialize(state)
+    @state = state
+    @neighbors = []
+  end
+
+  def continue?(cell)
+    neighbors = get_neighbor_count(cell)
+    if cell && neighbors > 1 && neighbors < 4
+      true
+    elsif neighbors === 3
+      false
+    end
+  end
+
+  def generate
+    next_gen = []
+    state.each do |cell|
+      next_gen << cell if continue? cell
+    end
+    next_gen
+  end
+
+  private
+  def get_neighbor_count(cell)
+    0
+  end
+
 end
--- test.rb
+++ test.rb
@@ -3,46 +3,42 @@ require_relative 'game_of_life'
 
 class TestGameOfLife < Test::Unit::TestCase
 
-  attr_reader :life, :cells
+  attr_reader :life
 
   def setup
-    @life = GameOfLife.new
     @cells = [
-      { x: 0, y: 0, neighbors: 0, },
-      { x: 1, y: 0, neighbors: 0, },
-      { x: 0, y: 1, neighbors: 0, },
-      { x: 1, y: 1, neighbors: 0, }
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 }
     ]
+    @life = GameOfLife.new(@cells)
   end
 
   def test_live_cell_continues_when_it_has_2_neighbors
-    cell = life.cell[0]
-    cell.neighbors = 2
-    assert(true, cell.continue)
+    cell = life.state[0]
+    p life.CELL[:create_id](cell)
+    assert( life.continue?(cell) )
   end
 
   def test_live_cell_continues_when_it_has_3_neighbors
-    cell = life.cell[0]
-    cell.neighbors = 3
-    assert(true, cell.continue)
+    cell = life.state[0]
+    assert( life.continue?(cell) )
   end
 
   def test_live_cell_dies_when_it_has_less_than_2_neighbors
-    cell = life.cell[0]
-    cell.neighbors = 1
-    assert(true, cell.die)
+    cell = life.state[0]
+    assert( !life.continue?(cell) )
   end
 
   def test_live_cell_dies_when_it_has_more_than_3_neighbors
-    cell = life.cell[0]
-    cell.neighbors = 4
-    assert(true, cell.die)
+    cell = life.state[0]
+    assert( !life.continue?(cell) )
   end
 
   def test_dead_cell_is_created_when_it_has_3_neighbors
-    cell = life.cell[0]
-    cell.neighbors = 2
-    assert(true, cell.continue)
+    cell = life.state[0]
+    assert( life.continue?(cell) )
   end
 
   def test_game_can_generate
