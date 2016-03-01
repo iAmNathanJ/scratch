d908e007fa1a7c1b4f7184a8e6d28ed19bae5eb8
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-01 13:59:42 -0500

    add notes for 3d to Cell class

--- cell.rb
+++ cell.rb
@@ -3,12 +3,14 @@ class Cell
   def initialize(coords)
     @x = coords[:x]
     @y = coords[:y]
+    # add z axis
   end
 
   def coords(offest_x = 0, offset_y = 0)
     {
       x: @x + offest_x,
       y: @y + offset_y
+      # add z axis
     }
   end
 
@@ -16,6 +18,7 @@ class Cell
     possible_cells = []
     for i in (-reach..reach)
       for j in (-reach..reach)
+        # add loop for z axis
         possible_cells << coords(i, j) unless (i == 0 && j == 0)
       end
     end
