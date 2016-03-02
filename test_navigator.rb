796457c85045e4084b856b8a2321b11a6ee54317
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-02 15:33:14 -0500

    add tests for navigator

--- test_navigator.rb
+++ test_navigator.rb
@@ -3,10 +3,10 @@ require_relative 'game_of_life'
 
 class TestGameOfLife < Test::Unit::TestCase
 
-  attr_reader :life
+  attr_reader :life, :locations
 
   def setup
-    state = [
+    @locations = [
       { x: 0, y: 0 }, # <- 2 neighbors, will remain
       { x: 1, y: 0 },
       { x: 0, y: 1 },
@@ -26,6 +26,31 @@ class TestGameOfLife < Test::Unit::TestCase
       { x: 26, y: 26 }
     ]
     @life = GameOfLife.new
+  end
+
+  def test_navigator_confirms_location_exists
+    result = life.living?({ x: 0, y: 0 }, locations)
+    assert_equal(true, result)
+  end
+
+  def test_navigator_counts_existing_neighbors_of_location
+    result = life.neighbors({ x: 0, y: 0 }, locations)
+    assert_equal(2, result)
+  end
+
+  def test_navigator_informs_of_locations_influence
+    result = life.influence({ x: 0, y: 0 })
+    expected = [
+      { x: -1, y: -1 },
+      { x: -1, y:  0 },
+      { x: -1, y:  1 },
+      { x:  0, y: -1 },
+      { x:  0, y:  1 },
+      { x:  1, y: -1 },
+      { x:  1, y:  0 },
+      { x:  1, y:  1 }
+    ]
+    assert_equal(expected, result)
   end
 
 end
