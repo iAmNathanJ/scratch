bfd6c7760a2c0dc196843b9f3b77c8f6f0af9459
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-02 15:18:58 -0500

    update test_rules

--- test_rules.rb
+++ test_rules.rb
@@ -9,4 +9,24 @@ class TestGameOfLife < Test::Unit::TestCase
     @life = GameOfLife.new
   end
 
+  def test_existing_location_doesnt_survive_if_less_than_three_neighbors
+    result = life.survivors(true, 2)
+    assert_equal(false, result)
+  end
+
+  def test_existing_location_persists_if_three_neighbors
+    result = life.survivors(true, 3)
+    assert_equal(true, result)
+  end
+
+  def test_existing_location_persists_if_four_neighbors
+    result = life.survivors(true, 4)
+    assert_equal(true, result)
+  end
+
+  def test_existing_location_doesnt_survive_if_more_than_four_neighbors
+    result = life.survivors(true, 5)
+    assert_equal(false, result)
+  end
+
 end
