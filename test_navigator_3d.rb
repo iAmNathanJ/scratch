fc3b35db9ac121dc9ce021988b913c4cd576317c
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-03 14:05:17 -0500

    add tests for 3d

---
+++ test_navigator_3d.rb
@@ -0,0 +1,62 @@
+require 'test/unit'
+require_relative 'navigator'
+
+class TestGameOfLife < Test::Unit::TestCase
+
+  attr_reader :locations, :nav
+
+  def setup
+    @locations = [
+      { x: 0, y: 0, z: 0 },
+      { x: 1, y: 0, z: 0 },
+      { x: 0, y: 1, z: 0 }
+    ]
+    @nav = XYZ_Navigator.new
+  end
+
+  def test_navigator_confirms_location_exists
+    result = nav.living?({ x: 0, y: 0, z: 0 }, locations)
+    assert_equal(true, result)
+  end
+
+  def test_navigator_counts_existing_neighbors_of_given_location
+    result = nav.neighbors({ x: 0, y: 0, z: 0 }, locations)
+    assert_equal(2, result)
+    result = nav.neighbors({ x: -1, y: 0, z: 0 }, locations)
+    assert_equal(2, result)
+  end
+
+  def test_navigator_informs_possible_influence_of_given_location
+    result = nav.influence({ x: 0, y: 0, z: 0 }, locations)
+    expected = [
+      { x: -1, y: -1, z: -1 },
+      { x: -1, y: -1, z:  0 },
+      { x: -1, y: -1, z:  1 },
+      { x: -1, y:  0, z: -1 },
+      { x: -1, y:  0, z:  0 },
+      { x: -1, y:  0, z:  1 },
+      { x: -1, y:  1, z: -1 },
+      { x: -1, y:  1, z:  0 },
+      { x: -1, y:  1, z:  1 },
+
+      { x:  0, y: -1, z: -1 },
+      { x:  0, y: -1, z:  0 },
+      { x:  0, y: -1, z:  1 },
+      { x:  0, y:  0, z: -1 },
+      { x:  0, y:  0, z:  1 },
+      { x:  0, y:  1, z: -1 },
+      { x:  0, y:  1, z:  1 },
+
+      { x:  1, y: -1, z: -1 },
+      { x:  1, y: -1, z:  0 },
+      { x:  1, y: -1, z:  1 },
+      { x:  1, y:  0, z: -1 },
+      { x:  1, y:  0, z:  1 },
+      { x:  1, y:  1, z: -1 },
+      { x:  1, y:  1, z:  0 },
+      { x:  1, y:  1, z:  1 }
+    ]
+    assert_equal(expected, result)
+  end
+
+end
