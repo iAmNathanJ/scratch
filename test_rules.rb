98bec9c1082d672abc3b4d69103484dd579e77c9
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-03 09:11:56 -0500

    update tests: rules is now class

--- test_rules.rb
+++ test_rules.rb
@@ -1,32 +1,37 @@
 require 'test/unit'
-require_relative 'game_of_life'
+require_relative 'rules'
 
 class TestGameOfLife < Test::Unit::TestCase
 
-  attr_reader :life
+  attr_reader :rules
 
   def setup
-    @life = GameOfLife.new
+    @rules = Rules.new
   end
 
-  def test_existing_location_doesnt_survive_if_less_than_three_neighbors
-    result = life.survivors(true, 2)
+  def test_existing_location_doesnt_survive_if_less_than_two_neighbors
+    result = rules.survivors(1)
     assert_equal(false, result)
+  end
+
+  def test_existing_location_persists_if_two_neighbors
+    result = rules.survivors(2)
+    assert_equal(true, result)
   end
 
   def test_existing_location_persists_if_three_neighbors
-    result = life.survivors(true, 3)
+    result = rules.survivors(3)
     assert_equal(true, result)
   end
 
-  def test_existing_location_persists_if_four_neighbors
-    result = life.survivors(true, 4)
-    assert_equal(true, result)
-  end
-
-  def test_existing_location_doesnt_survive_if_more_than_four_neighbors
-    result = life.survivors(true, 5)
+  def test_existing_location_doesnt_survive_if_more_than_three_neighbors
+    result = rules.survivors(4)
     assert_equal(false, result)
+  end
+
+  def test_nonexistent_location_comes_to_life_if_3_neighbors
+    result = rules.new_life(3)
+    assert_equal(true, result)
   end
 
 end
