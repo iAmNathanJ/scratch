ae9f1ae5ef2dabff65b28cf343e00eadf4bb03a1
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-03 09:07:41 -0500

    update nav tests: XY_Navigator is now a class

--- test_navigator.rb
+++ test_navigator.rb
@@ -1,9 +1,9 @@
 require 'test/unit'
-require_relative 'game_of_life'
+require_relative 'navigator'
 
 class TestGameOfLife < Test::Unit::TestCase
 
-  attr_reader :life, :locations
+  attr_reader :locations, :nav
 
   def setup
     @locations = [
@@ -11,31 +11,29 @@ class TestGameOfLife < Test::Unit::TestCase
       { x: 1, y: 0 },
       { x: 0, y: 1 }
     ]
-    @life = GameOfLife.new
+    @nav = XY_Navigator.new
   end
 
   def test_navigator_confirms_location_exists
-    result = life.living?({ x: 0, y: 0 }, locations)
+    result = nav.living?({ x: 0, y: 0 }, locations)
     assert_equal(true, result)
   end
 
-  def test_navigator_counts_existing_neighbors_of_location
-    result = life.neighbors({ x: 0, y: 0 }, locations)
+  def test_navigator_counts_existing_neighbors_of_given_location
+    result = nav.neighbors({ x: 0, y: 0 }, locations)
     assert_equal(2, result)
-    result = life.neighbors({ x: -1, y: 0 }, locations)
+    result = nav.neighbors({ x: -1, y: 0 }, locations)
     assert_equal(2, result)
   end
 
-  def test_navigator_informs_of_location_influence
-    result = life.influence({ x: 0, y: 0 }, locations)
+  def test_navigator_informs_possible_influence_of_given_location
+    result = nav.influence({ x: 0, y: 0 }, locations)
     expected = [
       { x: -1, y: -1 },
       { x: -1, y:  0 },
       { x: -1, y:  1 },
       { x:  0, y: -1 },
-      { x:  0, y:  1 },
       { x:  1, y: -1 },
-      { x:  1, y:  0 },
       { x:  1, y:  1 }
     ]
     assert_equal(expected, result)
