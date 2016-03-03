d485f5f0678bfa35be49f01305aa9a73fc357386
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-03 09:41:44 -0500

    update GameOfLife: renaming variables

--- game_of_life.rb
+++ game_of_life.rb
@@ -7,9 +7,8 @@ class GameOfLife
 
   def generate(locations)
     survivors = locations.select { |location| @rules.survivors(@navigate.neighbors(location, locations)) }
-    new_life = locations
-      .reduce([]) { |new_locations, location| new_locations + @navigate.influence(location, locations) }
-      .select { |location| @rules.new_life(@navigate.neighbors(location, locations)) }
+    affected = locations.reduce([]) { |new_locations, location| new_locations + @navigate.influence(location, locations) }
+    new_life = affected.select { |location| @rules.new_life(@navigate.neighbors(location, locations)) }
     survivors + new_life
   end
 
