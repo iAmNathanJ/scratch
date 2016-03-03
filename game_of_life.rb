5982a72a344d24de74d97db438e034de35a7b974
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-03 09:48:56 -0500

    update GameOfLife: clean up generate method, less confusing

--- game_of_life.rb
+++ game_of_life.rb
@@ -2,13 +2,24 @@ class GameOfLife
 
   def initialize(nav, rules)
     @navigate = nav
-    @rules = rules
+    @rule = rules
   end
 
   def generate(locations)
-    survivors = locations.select { |location| @rules.survivors(@navigate.neighbors(location, locations)) }
-    affected = locations.reduce([]) { |new_locations, location| new_locations + @navigate.influence(location, locations) }
-    new_life = affected.select { |location| @rules.new_life(@navigate.neighbors(location, locations)) }
+    survivors = locations.select do |location|
+      neighbors = @navigate.neighbors(location, locations)
+      @rule.survivors(neighbors)
+    end
+
+    affected = locations.reduce([]) do |new_locations, location|
+      new_locations + @navigate.influence(location, locations)
+    end
+
+    new_life = affected.select do |location|
+      neighbors = @navigate.neighbors(location, locations)
+      @rule.new_life(neighbors)
+    end
+
     survivors + new_life
   end
 
