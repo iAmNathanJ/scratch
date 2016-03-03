49d1240c909117815240368bf30d7f981c666709
iAmNathanJ <jacobs@heysparkbox.com>
2016-03-02 21:04:50 -0500

    update tests

--- test_life.rb
+++ test_life.rb
@@ -3,18 +3,18 @@ require_relative 'game_of_life'
 
 class TestGameOfLife < Test::Unit::TestCase
 
-  attr_reader :life, :locations
+  attr_reader :life
 
   def setup
-    @locations = [
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 0, y: 1 }
-    ]
     @life = GameOfLife.new
   end
 
   def test_generation
+    locations = [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 0, y: 1 }
+    ]
     result = life.generate(locations)
     expected = [
       { x: 0, y: 0 },
@@ -22,7 +22,22 @@ class TestGameOfLife < Test::Unit::TestCase
       { x: 0, y: 1 },
       { x: 1, y: 1 }
     ]
-    assert_equal(expected, result)
+    expected.each { |coords| assert(result.include?(coords)) }
+  end
+
+  def test_another_bc_whatever
+    locations = [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 }
+    ]
+    result = life.generate(locations)
+    expected = [
+      { x: 1, y: -1 },
+      { x: 1, y: 0 },
+      { x: 1, y: 1 }
+    ]
+    expected.each { |coords| assert(result.include?(coords)) } 
   end
 
 end
